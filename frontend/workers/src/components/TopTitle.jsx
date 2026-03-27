57fe88bfb1bc053515cdb2fd4dbbf6c28e064d67
Durgesh Khade <S24IT022@ms.pict.edu>
2026-03-27 14:56:14 +0530

    kiosk

--- frontend/workers/src/components/TopTitle.jsx
+++ frontend/workers/src/components/TopTitle.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function TopTitle() {
   return (
     <div className="fixed top-8 left-1/2 -translate-x-1/2 z-50">
-      <h1 className="font-headline text-2xl tracking-[0.5em] text-white/40 uppercase font-bold">NQA</h1>
+      <h1 className="font-headline text-2xl tracking-[0.5em] text-white/40 uppercase font-bold">KIOSK</h1>
     </div>
   );
 }
